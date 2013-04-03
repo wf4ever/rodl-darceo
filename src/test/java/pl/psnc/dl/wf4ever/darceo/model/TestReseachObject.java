932546f9609aabf14b8136b731017a230c50c352
pejot <pejot@pejot-pcsn-machine.(none)>
2013-04-03 13:38:13 +0200

    models covered by tests

--- src/test/java/pl/psnc/dl/wf4ever/darceo/model/TestReseachObject.java
+++ src/test/java/pl/psnc/dl/wf4ever/darceo/model/TestReseachObject.java
@@ -1,6 +1,48 @@
 package pl.psnc.dl.wf4ever.darceo.model;
 
+import java.net.URI;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
 
 public class TestReseachObject {
 
+    private URI roUri = URI.create("http://www.example.org/ROs/my_ro/");
+
+
+    @Test
+    public void testConstructor() {
+        ResearchObject ro = new ResearchObject(roUri);
+        Assert.assertNotNull(ro.getUri() != null);
+        Assert.assertNotNull(ro.getSerializables() != null);
+    }
+
+
+    @Test
+    public void testAddSerialziables() {
+        ResearchObject ro = new ResearchObject(roUri);
+        ro.addSerializable(new ResearchObjectComponent(roUri.resolve("component1"), getClass().getClassLoader()
+                .getResourceAsStream("mock/1.txt")));
+        ro.addSerializable(new ResearchObjectComponent(roUri.resolve("component2"), getClass().getClassLoader()
+                .getResourceAsStream("mock/2.txt")));
+        Assert.assertEquals(2, ro.getSerializables().size());
+    }
+
+
+    @Test
+    public void testAddSerialziablesUnique() {
+        ResearchObject ro = new ResearchObject(roUri);
+        ro.addSerializable(new ResearchObjectComponent(roUri.resolve("component1"), getClass().getClassLoader()
+                .getResourceAsStream("mock/1.txt")));
+        ro.addSerializable(new ResearchObjectComponent(roUri.resolve("component2"), getClass().getClassLoader()
+                .getResourceAsStream("mock/2.txt")));
+        ro.addSerializable(new ResearchObjectComponent(roUri.resolve("component1"), getClass().getClassLoader()
+                .getResourceAsStream("mock/1.txt")));
+        ro.addSerializable(new ResearchObjectComponent(roUri.resolve("component2"), getClass().getClassLoader()
+                .getResourceAsStream("mock/2.txt")));
+
+        Assert.assertEquals(2, ro.getSerializables().size());
+
+    }
 }
--- src/test/java/pl/psnc/dl/wf4ever/darceo/model/TestResearchObjectComponent.java
+++ src/test/java/pl/psnc/dl/wf4ever/darceo/model/TestResearchObjectComponent.java
@@ -1,6 +1,22 @@
 package pl.psnc.dl.wf4ever.darceo.model;
 
+import java.io.InputStream;
+import java.net.URI;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
 
 public class TestResearchObjectComponent {
 
+    private URI componentURI = URI.create("http://www.example.com/ROS/example_ro/component");
+
+
+    @Test
+    public void testConstructor() {
+        InputStream input = getClass().getClassLoader().getResourceAsStream("mock/1.txt");
+        ResearchObjectComponent component = new ResearchObjectComponent(componentURI, input);
+        Assert.assertEquals(componentURI, component.getUri());
+        Assert.assertEquals(input, component.getSerialization());
+    }
 }
